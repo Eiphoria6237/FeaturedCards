56c6a0b69cbee6d3e0634fdd8b76810f137b55b0
Eiphoria <liuerbsb@gmal.com>
2021-03-16 14:48:18 +0900

    add cards

--- M4C7/ContentView.swift
+++ M4C7/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cardcolor:Color {
+        return Color(.sRGB,
+                     red: Double.random(in: 0..<1), green: Double.random(in: 0..<1), blue: Double.random(in: 0..<1))
+        
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            
+            TabView {
+                ForEach(0..<50) { index in
+                    ZStack {
+                        
+                        Rectangle()
+                            .foregroundColor(cardcolor)
+                            .frame(width: geo.size.width-20, height: geo.size.height-50, alignment: .center)
+                            .cornerRadius(20)
+                            .shadow(radius: 10)
+                        Image(systemName: "\(index+1).circle")
+                            .resizable()
+                            .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                          
+                    }
+                    
+                }
+              
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+           
+        }
     }
 }
 
