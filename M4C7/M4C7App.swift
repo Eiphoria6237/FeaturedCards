3bca92148255855c82c85ba8c041e37d59f93507
Eiphoria <liuerbsb@gmal.com>
2021-03-16 14:12:15 +0900

    Initial Commit

---
+++ M4C7/M4C7App.swift
@@ -0,0 +1,17 @@
+//
+//  M4C7App.swift
+//  M4C7
+//
+//  Created by LIU SHURUI on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct M4C7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
